6d96750d5a9b9f5d06967314f11ac549ca68f185
lawrence1986 <madusquare@gmail.com>
2024-02-20 22:03:17 +0000

    fixed bugs

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -3,7 +3,7 @@
 
 
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
